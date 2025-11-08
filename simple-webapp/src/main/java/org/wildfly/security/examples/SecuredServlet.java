04d08e57c1203bfbae77bea8d6659b928621e1fe
Darran Lofthouse <darran.lofthouse@jboss.com>
2025-11-08 19:42:23 +0000

    Update the configuration to enable SPNEGO and update the servlet to log useful information.

--- simple-webapp/src/main/java/org/wildfly/security/examples/SecuredServlet.java
+++ simple-webapp/src/main/java/org/wildfly/security/examples/SecuredServlet.java
@@ -21,6 +21,8 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.security.Principal;
 
+import javax.security.auth.kerberos.KerberosTicket;
+
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.HttpMethodConstraint;
 import jakarta.servlet.annotation.ServletSecurity;
@@ -28,6 +30,13 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.ietf.jgss.GSSCredential;
+import org.ietf.jgss.GSSException;
+import org.wildfly.security.auth.server.IdentityCredentials;
+import org.wildfly.security.auth.server.SecurityDomain;
+import org.wildfly.security.auth.server.SecurityIdentity;
+import org.wildfly.security.credential.Credential;
+import org.wildfly.security.credential.GSSKerberosCredential;
 
 /**
  * A simple secured HTTP servlet.
@@ -50,6 +59,51 @@ public class SecuredServlet extends HttpServlet {
             Principal user = req.getUserPrincipal();
             writer.print(user != null ? user.getName() : "NO AUTHENTICATED USER");
             writer.print("'");
+            writer.println("    </p>");
+            writer.println("    <p>");
+            SecurityDomain securityDomain = SecurityDomain.getCurrent();
+            SecurityIdentity currentIdentity = securityDomain.getCurrentSecurityIdentity();
+            writer.print(" Current SecurityIdentity '");
+            writer.print(currentIdentity.getPrincipal().getName());
+            writer.print("<br>");
+            IdentityCredentials credentials = currentIdentity.getPrivateCredentials();
+            if (credentials.size() == 0) {
+                writer.print("NO PRIVATE CREDENTIALS");
+            } else {
+                writer.print("Private Credentials");
+                writer.print("<ul>");
+                for (Credential current : credentials) {
+                    writer.print("<li>" + current.getClass().getName());
+                    if (current instanceof GSSKerberosCredential) {
+                        GSSKerberosCredential gssKerbCred = (GSSKerberosCredential) current;
+                        GSSCredential gssCredential = gssKerbCred.getGssCredential();
+                        KerberosTicket ticket = gssKerbCred.getKerberosTicket();
+                        writer.print("<ul>");
+                            if (gssCredential != null) {
+                                try {
+                                    writer.print("<li>Name = " + gssCredential.getName().toString() + "</li>");
+                                    writer.print("<li>Remaining Lifetime = " + gssCredential.getRemainingLifetime() + "</li>");
+                                } catch (GSSException e) {
+                                    throw new IOException(e);
+                                }
+                            } else {
+                                writer.println("<li>GSSCredential is nullM</li>");
+                            }
+
+                            if (ticket != null) {
+                                writer.print("<li>KerberosClient =" + ticket.getClient().getName() + "</li>");
+                                writer.print("<li>KerberosServer =" + ticket.getServer().getName() + "</li>");
+                                writer.print("<li>End Time = " + ticket.getEndTime().toString() + "</li>");
+                            } else {
+                                writer.println("<li>KerberosTicket is null</li>");
+                            }
+                        writer.print("</ul>");
+                    }
+                    writer.print("</li>");
+                }
+                writer.print("</ul>");
+            }
+
             writer.println("    </p>");
             writer.println("  </body>");
             writer.println("</html>");
